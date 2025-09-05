8ce9ae151607b513a89461189844f27654d16250
jasmin.martens <papoujuice@gmail.com>
2025-09-05 12:04:08 +0300

    assignment 1 done

---
+++ assignment-1/script.js
@@ -0,0 +1,50 @@
+const titleBtn = document.querySelector("#my-button");
+const listBtn = document.querySelector("#add-data");
+const textContent = document.querySelector("#textToCopy");
+const textBtn = document.querySelector("#addTextBtn");
+const list = document.querySelector("#my-list");
+const headline = document.querySelector("h1");
+
+titleBtn.addEventListener("click", () => {
+    console.log("hello world");
+    headline.innerHTML = "Moi maailma";
+});
+
+listBtn.addEventListener("click", () => {
+    const babyListEl = document.createElement("li");
+
+    let date = new Date();
+    date.toDateString();
+    babyListEl.innerHTML = "I was born on " + date;
+
+    list.appendChild(babyListEl);
+});
+
+function copyText() {
+    const copyListEl = document.createElement("li");
+
+    copyListEl.innerHTML = textContent.value;
+
+    list.appendChild(copyListEl);
+    textContent.value = "";
+    textBtn.disabled = true;
+}
+
+function checkEmpty() {
+    const content = textContent.value.trim();
+    if (content == "") {
+        return true;
+    }
+}
+
+textContent.addEventListener("keyup", () => {
+    if (checkEmpty()) {
+        textBtn.disabled = true;
+    } else {
+        textBtn.disabled = false;
+    }
+});
+
+textBtn.addEventListener("click", () => {
+    copyText();
+});
